f346beeae335fc40436122d640dea3cafab13bb2
DaniilKrotenko <danya09.krem@gmail.com>
2023-01-17 19:08:18 +0300

    Update productPage.blade.php

--- resources/views/productPage.blade.php
+++ resources/views/productPage.blade.php
@@ -4,7 +4,7 @@
 @foreach ($info as $i)
 <div class="container px-4 px-lg-5 my-5">
     <div class="row gx-4 gx-lg-5 align-items-center">
-        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{$i->image}}" alt="..."/></div>
+        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="{{'storage/img/'.$i->image}}" alt="..."/></div>
         <div class="col-md-6">
             <div class="small mb-1">{{$nameCategory}}</div>
             <h1 class="display-5 fw-bolder">{{$i->name}}</h1>
